237f36ca5d771004413c3ac3714a1cbdd321c298
Kubi Kubisiak <mckubisiak@gmail.com>
2021-05-13 10:07:17 -0700

    updated order button to clear content

--- cart/cart.js
+++ cart/cart.js
@@ -2,6 +2,7 @@ import { renderCart, getCartTotal } from './render-cart.js';
 // import { cartItems } from '../data/cart-items.js';
 import { getCart } from '../local-storage-utils.js';
 import { reload } from '../utils.js';
+import marbles from '../data/marbles.js';
 
 const anchor = document.querySelector('tbody');
 const total = document.getElementById('total');
@@ -24,12 +25,11 @@ total.textContent = `€ ${cartTotal}`;
 
 orderButton.addEventListener('click', () => {
 
-
     alert(` your order has been place: ${JSON.stringify(cartItems)}`);
     console.log(cartItems);
     console.log(typeof cartItems);
     localStorage.clear();
-    javascript: reload();
+    location.reload();
     
     return;
 });
